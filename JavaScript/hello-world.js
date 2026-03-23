3ce8f576a3d6fe5b514fdaffbefc619722f629e4
iitssneree <nereagago18.2@gmail.com>
2026-03-23 21:10:34 +0100

    Inicios con Java

---
+++ JavaScript/hello-world.js
@@ -0,0 +1,19 @@
+console.log('Hello world!');
+let counter = 0;
+console.log(counter);
+console.log(typeof counter);
+
+counter = 4.5;
+console.log('-------------------------------------------');
+console.log(counter);
+console.log(typeof counter);
+
+counter = "John Doe";
+console.log('-------------------------------------------');
+console.log(counter);
+console.log(typeof counter);
+
+counter = false; 
+console.log('-------------------------------------------');
+console.log(counter);
+console.log(typeof counter);
